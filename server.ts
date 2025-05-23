b60331edf043d6a01e94ab7d1163d502331a7a5e
a.grachev@rwdt.ru <a.grachev@rwdt.ru>
2025-05-23 12:18:39 +0300

    upd port

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const port = 3000;
+const port = 3009;
 
 app.get('/', (req, res) => {
   console.log('!!!');
--- server2.ts
+++ server2.ts
@@ -1,6 +1,6 @@
 setInterval(() => {
   // fetch('http://127.0.0.1:3000/')
-  fetch('http://host.docker.internal:3000/')
+  fetch('http://host.docker.internal:3009/')
     .then((res) => res.text())
     .then((res) => console.log(res));
 }, 2000);
