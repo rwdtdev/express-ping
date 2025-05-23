93f25095e820bcdf46d290ad8e8b288098c17da7
a.grachev@rwdt.ru <a.grachev@rwdt.ru>
2025-05-23 12:29:10 +0300

    add localhost

--- server2.ts
+++ server2.ts
@@ -1,6 +1,7 @@
 setInterval(() => {
   // fetch('http://127.0.0.1:3009/')
-  fetch('http://host.docker.internal:3009/')
+  // fetch('http://host.docker.internal:3009/')
+  fetch('http://localhost:3009/')
     .then((res) => res.text())
     .then((res) => console.log(res));
 }, 2000);
